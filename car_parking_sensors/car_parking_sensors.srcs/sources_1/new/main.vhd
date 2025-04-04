63a14eec53b85cd8a22a6e90758a659eee868ce5
karel-matousek <k.matousek20@gmail.com>
2025-04-04 09:55:41 +0200

    Add files via upload

---
+++ car_parking_sensors/car_parking_sensors.srcs/sources_1/new/main.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04.04.2025 09:51:36
+-- Design Name: 
+-- Module Name: main - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity main is
+    Port ( echo : in STD_LOGIC;
+           trig : out STD_LOGIC);
+end main;
+
+architecture Behavioral of main is
+
+begin
+
+
+end Behavioral;
