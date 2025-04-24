5eae8d14d5ef92f4aae5ba321b99c8e25dcc05e3
Pavel025 <kipap04@gmail.com>
2025-04-24 22:29:18 +0200

    beep_v_01

---
+++ car_parking_sensors/car_parking_sensors.srcs/sim_1/new/tb_Buzzer.vhd
@@ -0,0 +1,74 @@
+-- Testbench automatically generated online
+-- at https://vhdl.lapinoo.net
+-- Generation date : Thu, 24 Apr 2025 18:59:40 GMT
+-- Request id : cfwk-fed377c2-680a8a1cceeb0
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tb_Buzzer is
+end tb_Buzzer;
+
+architecture tb of tb_Buzzer is
+
+    component Buzzer
+        port (clk  : in std_logic;
+              dist : in std_logic_vector (14 downto 0);
+              beep : out std_logic);
+    end component;
+
+    signal clk  : std_logic;
+    signal dist : std_logic_vector (14 downto 0);
+    signal beep : std_logic;
+
+    constant TbPeriod : time := 10 ns; 
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : Buzzer
+    port map (clk  => clk,
+              dist => dist,
+              beep => beep);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- ***EDIT*** Check that clk is really your main clock signal
+    clk <= TbClock;
+
+    stimuli : process
+    begin
+        -- ***EDIT*** Adapt initialization as needed
+        dist <= (others => '0');
+        wait for 10 ns;
+        -- Reset generation
+        --  ***EDIT*** Replace YOURRESETSIGNAL below by the name of your reset as I haven't guessed it
+        dist <= std_logic_vector(to_unsigned(2000, 15));
+        wait for 400 ns;
+        dist <= std_logic_vector(to_unsigned(1000, 15));
+        wait for 400 ns;
+        dist <= std_logic_vector(to_unsigned(4000, 15));
+        wait for 400 ns;
+        dist <= std_logic_vector(to_unsigned(8000, 15));
+        wait for 400 ns;
+        dist <= std_logic_vector(to_unsigned(10000, 15));
+        wait for 200 ns;
+        -- ***EDIT*** Add stimuli here
+       
+
+        -- Stop the clock and hence terminate the simulation
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_Buzzer of tb_Buzzer is
+    for tb
+    end for;
+end cfg_tb_Buzzer;
---
+++ car_parking_sensors/car_parking_sensors.srcs/sources_1/new/Buzzer.vhd
@@ -0,0 +1,67 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity Buzzer is
+    Port (
+        clk   : in  std_logic;
+        dist  : in  STD_LOGIC_VECTOR(14 downto 0);  -- 32 767 ~ 4 m
+        beep  : out STD_LOGIC
+    );
+end Buzzer;
+
+architecture Behavioral of Buzzer is
+    constant CLK_FREQ : integer := 10; -- _000_000 -- 100 M = 1 s => 10 ns  !!!!!!!!!
+
+    signal dist_internal         : unsigned(14 downto 0);
+    signal interval       : integer := 0;  -- ON + OFF
+    signal beep_duration  : integer := 0;  -- ON
+    signal counter        : integer := 0;
+    signal beep_state     : std_logic := '0';
+begin
+
+    dist_internal <= unsigned(dist);
+
+    process(clk)
+    begin
+        if rising_edge(clk) then
+
+            -- délky intervalů
+            if dist_internal < to_unsigned(2047, 15) then      -- 0.25 m
+                interval      <= 2*CLK_FREQ;  -- 2 s
+                beep_duration <= CLK_FREQ;  
+            elsif dist_internal < to_unsigned(4095, 15) then  -- 0.5 m
+                interval      <= 1*CLK_FREQ;   
+                beep_duration <= CLK_FREQ/2;   
+            elsif dist_internal < to_unsigned(8190, 15) then -- 1 m
+                interval      <= CLK_FREQ/2;   
+                beep_duration <= CLK_FREQ/2;   -- dat 4 !!!!!!!!
+            else
+                interval      <= 0;       -- OFF
+                beep_duration <= 0;
+            end if;
+
+            -- beep
+            if interval = 0 then
+                beep <= '0';
+                counter <= 0;
+                beep_state <= '0';
+            else
+                if counter < interval then
+                    counter <= counter + 1;
+                else
+                    counter <= 0;
+                end if;
+
+                if counter < beep_duration then
+                    beep_state <= '1';  -- beep ON
+                else
+                    beep_state <= '0';  -- beep OFF
+                end if;
+
+                beep <= beep_state;
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
