eba32df603f5f8e36e9fecccee8127d0cc644898
Pavel025 <kipap04@gmail.com>
2025-04-29 21:31:15 +0200

    repaired beep, clk, dist in cm

--- car_parking_sensors/car_parking_sensors.srcs/sources_1/new/Buzzer.vhd
+++ car_parking_sensors/car_parking_sensors.srcs/sources_1/new/Buzzer.vhd
@@ -2,18 +2,18 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity Buzzer is
+entity Buzzer is    --!!!!!!!!!!!!!
     Port (
         clk   : in  std_logic;
-        dist  : in  STD_LOGIC_VECTOR(14 downto 0);  -- 32 767 ~ 4 m
+        dist  : in  STD_LOGIC_VECTOR(8 downto 0);  -- 32 767 ~ 4 m              --!!!!!!!!!!!!! v top levlu nastavit 8 downto 0 i u led
         beep  : out STD_LOGIC
     );
-end Buzzer;
+end Buzzer;     --!!!!!!!!!!!!!
 
-architecture Behavioral of Buzzer is
-    constant CLK_FREQ : integer := 10_000_000; -- -- 100 M = 1 s => 10 ns  !!!!!!!!!
+architecture Behavioral of Buzzer is        --!!!!!!!!!!!!!
+    constant CLK_FREQ : integer := 100_000_000; -- 100 M = 1 s => 10 ns 
 
-    signal dist_internal         : unsigned(14 downto 0);
+    signal dist_internal         : unsigned(8 downto 0);
     signal interval       : integer := 0;  -- ON + OFF
     signal beep_duration  : integer := 0;  -- ON
     signal counter        : integer := 0;
@@ -30,15 +30,15 @@ begin
         if rising_edge(clk) then
 
             -- délky intervalů
-            if dist_internal < to_unsigned(8190, 15) then      -- 0.25 m
-                interval      <= 2*CLK_FREQ;  -- 2 s
-                beep_duration <= CLK_FREQ;  
-            elsif dist_internal < to_unsigned(4095, 15) then  -- 0.5 m
-                interval      <= 1*CLK_FREQ;   
-                beep_duration <= CLK_FREQ/2;   
-            elsif dist_internal < to_unsigned(2047, 15) then -- 1 m
+            if dist_internal < to_unsigned(25, 8) then     --   z. z. z. z.
                 interval      <= CLK_FREQ/2;   
+                beep_duration <= CLK_FREQ/4;  
+            elsif dist_internal < to_unsigned(50, 8) then  --   z. .. z. ..
+                interval      <= CLK_FREQ;   
                 beep_duration <= CLK_FREQ/4;   
+            elsif dist_internal < to_unsigned(100, 8) then  --  z. .. .. ..
+                interval      <= 2*CLK_FREQ;  
+                beep_duration <= CLK_FREQ/4; 
             else
                 interval      <= 0;       -- OFF
                 beep_duration <= 0;
